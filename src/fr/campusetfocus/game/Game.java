ee7fd6ec31ee2858dca795d69f69760afc536a10
guillaume-desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-08-14 16:55:39 +0200

    add menu info player

--- src/fr/campusetfocus/game/Game.java
+++ src/fr/campusetfocus/game/Game.java
@@ -26,8 +26,9 @@ public class Game {
     public Character getPlayer() {
         return player;
     }
+
     public void setPlayer(Character player) {
-       this.player = player;
+        this.player = player;
     }
 
     public Dice getDice() {
@@ -37,17 +38,14 @@ public class Game {
     public Menu getMenu() {
         return menu;
     }
-    public void setMenu(Menu menu) {
-        this.menu = menu;
-    }
 
-    public void start(){
+    public void start() {
 
         this.menu.display("""
-                        === === === === === === === === ===
-                        === Bienvenu sur Campus & Focus ===
-                        === === === === === === === === ===
-                        """);
+                === === === === === === === === ===
+                === Bienvenu sur Campus & Focus ===
+                === === === === === === === === ===
+                """);
 
         int choice = this.menu.getChoice("=== === Menu principal === ===", new String[]{"Nouvelle Partie", "Quitter"});
 
@@ -67,33 +65,52 @@ public class Game {
 
     public void createNewPlayer() {
 
-            this.menu.display("""
-                    === === Création de votre personnage === ===
-                    """);
+        this.menu.display("""
+                === === Création de votre personnage === ===
+                """);
 
-            int choice = this.menu.getChoice("Choisissez le type de votre personnage :", new String[]{"Guerrier", "Magicien"});
-            String type ="";
+        int choice = this.menu.getChoice("Choisissez le type de votre personnage :", new String[]{"Guerrier", "Magicien"});
+        String type = "";
 
-            switch (choice) {
-                case 1:
-                    type = "Warrior";
-                    break;
-                case 2:
-                    type = "Magus";
-                    break;
-                default:
-                    this.menu.display("Erreur inconnue");
-                    this.createNewPlayer();
-            }
+        switch (choice) {
+            case 1:
+                type = "Warrior";
+                break;
+            case 2:
+                type = "Magus";
+                break;
+            default:
+                this.menu.display("Erreur inconnue");
+                this.createNewPlayer();
+        }
 
-            String name = this.menu.getString("Entrez le nom de votre personnage :");
+        String name = this.menu.getString("Entrez le nom de votre personnage :");
 
-            this.player = new Character(name, type);
+        this.player = new Character(name, type);
 
-            this.menu.display("Votre personnage est créé.");
-            this.menu.display(this.player.toString());
+        this.menu.display("Votre personnage est créé.");
+        this.playerCreated();
+    }
+
+    public void playerCreated() {
+        int choice = this.menu.getChoice("=== === Menu Personnage === ===", new String[]{"Afficher infos", "Modifier le personnage", "Retour au menu principal"});
+
+        switch (choice) {
+            case 1:
+                this.menu.display(this.player.toString());
+                this.playerCreated();
+                break;
+            case 2:
+                this.createNewPlayer();
+                break;
+            case 3:
+                this.start();
+            default:
+                this.menu.display("Erreur inconnue");
+                this.playerCreated();
         }
     }
+}
 
 
 
